09012e588af6ffb3a0746edc929a364418c6d9f3
usuario <usuario@pc21-089>
2023-10-03 16:35:07 +0200

    Committing test

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,22 @@
 
 public class Calculadora {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int suma(int a, int b) {
+		return (a+b);
+	}
+	
+	public static int resta (int a, int b) {
+		return (a-b);
+	}
+	
+	public static int multiplicacion (int a, int b) {
+		return (a*b);
+	}
+	
+	public static int divide (int a, int b) {
+		if(b==0) {
+			return 0;
+		}else	return (a/b);
 	}
 
 }
