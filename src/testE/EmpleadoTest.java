92473c23bfa03c8ec773d1b8b402331d5e1c3a10
Clod-ai <chiri.claudio73@gmail.com>
2023-10-07 20:02:29 +0200

    Casos de prueba: Definidos y en prueba

--- src/testE/EmpleadoTest.java
+++ src/testE/EmpleadoTest.java
@@ -10,8 +10,13 @@ import org.junit.jupiter.api.Test;
 
 class EmpleadoTest {
 
+	public enum TipoEmpleado{Vendedor, Encargado, Otro};
+	public float ventasMes;
+	public float horasExtra;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		
 	}
 
 	@AfterAll
@@ -20,6 +25,10 @@ class EmpleadoTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		TipoEmpleado tipo = Encargado;
+		ventasMes = 1000;
+		horaExtra = 2;
+		
 	}
 
 	@AfterEach
@@ -28,67 +37,144 @@ class EmpleadoTest {
 
 	@Test
 	void testCalculaNominaBrutaEncargado() {
-		fail("Not yet implemented");
+		TipoEmpleado tipo = Encargado;
+		int expected = 2,660;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		assertEquals(expected,actaul);
 	}
 
 	@Test
 	void testCalculoNominaNetaVendedor() {
-		fail("Not yet implemented");
+		TipoEmpleado tipo = Vendedor;
+		int expected = 2,160;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		assertEquals(expected,actaul);
 	}
 	
 	@Test
 	void testCalculaNominaBrutaOtro() {
-		fail("Not yet implemented");
+		TipoEmpleado tipo = Otro;
+		int expected = 60;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		assertEquals(expected,actaul);
 	}
 
 	@Test
 	void testCalculaNominaBrutaVentasNegativa() {
-		fail("Not yet implemented");
+
+		ventasMes = -50;
+		
+		int expected = 2,560;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		
+		assertEquals(expected,actaul);
 	}
 	
 	@Test
 	void testCalculaNominaBrutaVentasMesMenos1000() {
-		fail("Not yet implemented");
+		
+		ventasMes = 999;
+		
+		int expected = 2,560;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		
+		assertEquals(expected,actaul);
 	}
 
 	@Test
 	void testCalculaNominaBrutaVentasMesEntre1000y1500() {
-		fail("Not yet implemented");
+
+		ventasMes = 1250;
+		
+		int expected = 2,660;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		
+		assertEquals(expected,actaul);
 	}
 	
 	@Test
 	void testCalculaNominaBrutaVentasMesMayor1500() {
-		fail("Not yet implemented");
+
+		ventasMes = 1750;
+		
+		int expected = 2,760;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		
+		assertEquals(expected,actaul);
 	}
 	
 	@Test
 	void testCalculaNominaBrutaHorasExtraPositivas() {
-		fail("Not yet implemented");
+		
+		horaExtra = 2;
+		
+		int expected = 2,660;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		
+		assertEquals(expected,actaul);
 	}
 	
 	@Test
 	void testCalculaNominaBrutaHorasExtraNegativas() {
-		fail("Not yet implemented");
+		
+		horaExtra = -2;
+		
+		int expected = 2,540;
+		int actaul = calculaNominaBruta(tipo, ventasMes, horaExtra);
+		
+		assertEquals(expected,actaul);
 	}
 
 	@Test
 	void testCalculoNominaNetaNegativa() {
-		fail("Not yet implemented");
+		
+		TipoEmpleado tipo = Otro;
+		horaExtra = -2;
+		ventasMes = 500;
+		
+		int expected = -60;
+		int actaul = calculoNominaNeta(calculaNominaBruta(tipo, ventasMes, horaExtra));
+		
+		assertEquals(expected,actaul);
 	}
 	
 	@Test
 	void testCalculoNominaNetaMenos2100() {
-		fail("Not yet implemented");
+		
+		TipoEmpleado tipo = Vendedor;
+		horaExtra = 0;
+		ventasMes = 500;
+		
+		int expected = 2000;
+		int actaul = calculoNominaNeta(calculaNominaBruta(tipo, ventasMes, horaExtra));
+		
+		assertEquals(expected,actaul);
 	}
 	
 	@Test
 	void testCalculoNominaNetaEntre2100y2500() {
-		fail("Not yet implemented");
+		
+		TipoEmpleado tipo = Vendedor;
+		horaExtra = 1;
+		ventasMes = 1000;
+		
+		int expected = 1,836;
+		int actaul = calculoNominaNeta(calculaNominaBruta(tipo, ventasMes, horaExtra));
+		
+		assertEquals(expected,actaul);
 	}
 	
 	@Test
 	void testCalculoNominaNetaMayor2500() {
-		fail("Not yet implemented");
+		
+		TipoEmpleado tipo = Encargado;
+		horaExtra = 0;
+		ventasMes = 1000;
+		
+		int expected = 2,132;
+		int actaul = calculoNominaNeta(calculaNominaBruta(tipo, ventasMes, horaExtra));
+		
+		assertEquals(expected,actaul);
 	}
 
 }
